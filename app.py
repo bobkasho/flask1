e3f0735e7a8a60a3d5edf22cf4c2ddc24090020d
Bob <vladimir@kashir.in>
2023-07-13 23:41:33 +0300

    Щось я загруз в venv. Розгрібаю, треба виїзджаити на новий рівень

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article $r>' % self.id
 
 @app.route('/')
+@app.route('/home')
 def index():
     return "Hello World"
+
+
+@app.route('/about')
+def about():
+    return "About page"
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page: " + str.title(name) + " - " + str(id)
 
 if __name__ == "__main__":
     app.run(debug=True)
